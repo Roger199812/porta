a299371551d67bb95c76c20a55fdcf3f07ec59d5
Roger199812 <81428629+Roger199812@users.noreply.github.com>
2021-07-04 12:27:24 -0500

    Add files via upload

---
+++ porta3/scrip.js
@@ -0,0 +1,10 @@
+let  nombre = 'roger'//comilla simple
+let apellidos= "gonzales"//  comilla doble
+
+les saludo =  `hola me llamo ${nombre }${apellido}
+let cadena "cadena"
+ //let cadena = new String("cadena ")
+
+console.long(nombre,apellidos)
+console.long(saludos)
+console.long(cadena )
